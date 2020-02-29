0fde9a11b3299626ce639989cb2e3ff39ec8ff97
Jasmine Qin <yijia.qin@alumni.ubc.ca>
2020-02-28 17:03:39 -0800

    second check

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,13 @@
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
